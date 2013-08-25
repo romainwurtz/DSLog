8a11f8428fcecd3e8c7e6f5350872023e88df61f
Romain Wurtz <romain.wurtz@gmail.com>
2013-08-25 12:07:43 -0400

    First commit

---
+++ DSlog.h
@@ -0,0 +1,17 @@
+//
+//  DSlog.h
+//
+//  Created by Romain Wurtz on 8/25/13.
+//  Copyright (c) 2013 Romain Wurtz. All rights reserved.
+//
+
+#ifdef DEBUG
+#define DSLogError(...) NSLog(@"[%@][%s (%@:%i)][ERROR] %@", ([[NSThread currentThread] isMainThread] ? @"Main Thread" : [NSString stringWithFormat:@"Thread %p", [NSThread currentThread]]), __func__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent] , __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#define DSLog(...) NSLog(@"[%@][%s (%@:%i)] %@", ([[NSThread currentThread] isMainThread] ? @"Main Thread" : [NSString stringWithFormat:@"Thread %p", [NSThread currentThread]]), __func__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent] , __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#define DSLogInfo(...) NSLog(@"[%@][%s (%@:%i)][INFO] %@", ([[NSThread currentThread] isMainThread] ? @"Main Thread" : [NSString stringWithFormat:@"Thread %p", [NSThread currentThread]]), __func__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent] , __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#else
+#define DSLog(...) do {} while(0)
+#define DSLogError(...) do {} while(0)
+#define DSLogInfo(...) do {} while(0)
+// #define NSLog(...) /* */
+#endif
