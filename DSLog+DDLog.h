8e2fd63c8d398f4212c769f1acf5eda743f6f076
Romain Wurtz <romain.wurtz@gmail.com>
2013-08-26 11:36:33 -0400

    do not deactivate ddlog on release product

--- DSLog+DDLog.h
+++ DSLog+DDLog.h
@@ -11,9 +11,5 @@
 #define DSLogInfo(...) DDLogInfo(@"[%@][%s (%@:%i)] %@", ([[NSThread currentThread] isMainThread] ? @"Main Thread" : [NSString stringWithFormat:@"Thread %p", [NSThread currentThread]]), __func__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent] , __LINE__, [NSString stringWithFormat:__VA_ARGS__])
 #define DSLogWarn(...) DDLogWarn(@"[%@][%s (%@:%i)] %@", ([[NSThread currentThread] isMainThread] ? @"Main Thread" : [NSString stringWithFormat:@"Thread %p", [NSThread currentThread]]), __func__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent] , __LINE__, [NSString stringWithFormat:__VA_ARGS__])
 #else
-#define DSLog(...) ((void)0)
-#define DSLogError(...) ((void)0)
-#define DSLogInfo(...) ((void)0)
-#define DSLogWarn(...) ((void)0)
 // #define NSLog(...) /* */
 #endif
